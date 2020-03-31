ff89114fc9e22105b0a7e52aba5e4aa875a812ce
Matheus Gois <matheusmaxi9.0@gmail.com>
2020-03-31 00:00:29 -0300

    Atualização 31/03

--- PELTIER/control_digitalpot/control_digitalpot.ino
+++ PELTIER/control_digitalpot/control_digitalpot.ino
@@ -1,14 +1,14 @@
 // Valores do Potenciometro
-#define POT_VALUE 10000L        // Nominal POT value
-#define STEP_OHMS POT_VALUE/99  // Number of ohms per tap point
-#define PULSE_TIMED 10          // millisecond delay 
+#define PULSE_TIMED       10                    // millisecond delay 
+#define SERIAL_BAUD_RATE  9600                  // Baud rate to the serial
+
 
 // Variáveis
-const int pinUD = 8;            // --> X9C103P pin 2
-const int pinINC = 9;           // --> X9C103P pin 1
-const int CS = 10;              // --> X9C103P pin 7
-const int pinWiper = A1;        // --> X9C103P pin 5
-const int tapPoints = 100;     // X9C103P specs
+const int pinUD = 8;                            // --> X9C103P pin 2
+const int pinINC = 9;                           // --> X9C103P pin 1
+const int CS = 10;                              // --> X9C103P pin 7
+const int ohms = 10;
+const int temp = 1000;
 
 void setup() {
   // Definindo Entradas e Saída
@@ -20,45 +20,53 @@ void setup() {
   digitalWrite(CS, HIGH);
 
   // Iniciando serial
-  Serial.begin(9600);
-  Serial.println("Testando o Potenciometro Digital");
+  Serial.begin(SERIAL_BAUD_RATE);
+  Serial.println("Iniciando Controle do Pontenciometro... \n");  
   
-
-
 }
 
-void X9C103P_INC(int enable, int pulse){
-
-  digitalWrite(pulse, HIGH);   // HIGH before falling edge
-                               // Not recommended for puksed key to be low 
-                               // when chip select (enable) pulled low.
-  delay(PULSE_TIMED);          // wait for IC/stray capacitance ?
-  digitalWrite(enable,LOW);    // select the POT
-  digitalWrite(pulse, LOW);    // LOW for effective falling edge
-  delay(PULSE_TIMED);          // wait for IC/stray capacitance ?
-                               // tap point copied into non-volatile memory
-                               // if CS returns HIGH while INC is HIGH
-  digitalWrite(enable,HIGH);   //deselect the POT 
-
+void X9C103P_INC() {
+  // High to prepare the falling edge
+  digitalWrite(pinINC, HIGH);                     // HIGH before falling edge
+  delay(PULSE_TIMED);                             // wait for IC/stray capacitance
+  digitalWrite(CS, LOW);                          // select the POT
+  digitalWrite(pinINC, LOW);                      // LOW for effective falling edge
+  delay(PULSE_TIMED);                             // wait for IC/stray capacitance
+  digitalWrite(CS, HIGH);                         // standby mode
 }
 
 void loop() {
   if (Serial.available() > 0) {
     char value = Serial.read();
-    if (value == 'z') {
-      // Move the tap point one count
-      digitalWrite(pinUD, true);
-      X9C103P_INC(CS, pinINC);
-      delay(200);     // delay a bit for viewing
-      Serial.println("inc");
-    }if(value == 'x'){
-      digitalWrite(pinUD, false);
-      X9C103P_INC(CS, pinINC);
-      delay(200);     // delay a bit for viewing
-      Serial.println("dec");
+    switch (value) {
+      case 'u':                                     // u - up
+        while (1) {
+          digitalWrite(pinUD, LOW);                  // With the U/D pin HIGH, the mode is Wiper UP
+          for (int i = 1; i <= ohms; i++)X9C103P_INC();                              // Manage the pins state to increment
+          delay(temp);                       // delay a bit for viewing
+          Serial.println("Incrementando...");         // show that is incrementing
+          value = Serial.read();
+          if (value == 's'){
+            Serial.println("Stop!");
+            break;
+          }
+        }
+        break;
+      case 'd':                                     // d - down
+        while (1) {
+          digitalWrite(pinUD, HIGH);                   // With the U/D pin HIGH, the mode is Wiper UP
+          for (int j = 1; j <= ohms; j++)X9C103P_INC();                              // Manage the pins state to decrement
+          delay(temp);                       // delay a bit for viewing
+          Serial.println("Decrementando...");         // show that is decrementing
+          value = Serial.read();
+          if (value == 's'){
+            Serial.println("Stop!");
+            break;
+          }
+        }        
+        break;
+      default:
+        Serial.println("Opção inválida.");
     }
   }
-  Serial.print(".");
-  delay(1000);
-
 }
